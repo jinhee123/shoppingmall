8b5b2eb8bb1cb80e9dcf4535635460d0c15b804f
jinhee123 <peaceful12calm12@gmail.com>
2020-12-19 01:01:09 +0900

    init

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { FaCode } from "react-icons/fa";
+import axios from "axios";
+
+function LandingPage() {
+    useEffect(() => {
+
+        axios.post('/api/product/products',)
+        .then(response => {
+            if(response.data.success){
+                console.log(response.data)
+            }else {
+                alert("상품을 가져오는데 실패했습니다.")
+            }
+        })
+    },[])
+
+
+    return (
+        <div>
+            hi
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ server/config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://jinhee1234:jinhee1234@cluster0.lzd2n.mongodb.net/test?retryWrites=true&w=majority'
+}
